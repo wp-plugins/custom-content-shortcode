3210576263ddcc4b0239126064fa794a20fcc1ba
miyarakira <miyarakira@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-09-27 04:57:40 +0000

    edited readme.txt
    git-svn-id: https://plugins.svn.wordpress.org/custom-content-shortcode/tags/0.1.9@779089 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- custom-content-shortcode.php
+++ custom-content-shortcode.php
@@ -1571,14 +1571,15 @@ class custom_bootstrap_navwalker extends Walker_Nav_Menu {
 			 * Since the the menu item is NOT a Divider or Header we check the see
 			 * if there is a value in the attr_title property. If the attr_title
 			 * property is NOT null we apply it as the class name for the glyphicon.
-			 */
 
 			if(! empty( $item->attr_title )){
 				$item_output .= '<a'. $attributes .'><span class="glyphicon ' . esc_attr( $item->attr_title ) . '"></span>&nbsp;';
 			} else {
 				$item_output .= '<a'. $attributes .'>';
 			}
-			
+			 */
+
+			$item_output .= '<a'. $attributes .'>';
 			$item_output .= $args->link_before . apply_filters( 'the_title', $item->title, $item->ID ) . $args->link_after;
 			$item_output .= ($args->has_children && $depth === 0) ? ' <span class="caret"></span></a>' : '</a>';
 			$item_output .= $args->after;
