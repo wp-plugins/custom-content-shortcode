600961abbfdd2670c0ae7adc537908aa36169a15
miyarakira <miyarakira@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-04-30 07:44:31 +0000

    tagging 0.8.8
    git-svn-id: https://plugins.svn.wordpress.org/custom-content-shortcode/tags/0.8.8@905389 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- custom-content-shortcode.php
+++ custom-content-shortcode.php
@@ -3,10 +3,13 @@
 Plugin Name: Custom Content Shortcode
 Plugin URI: http://wordpress.org/plugins/custom-content-shortcode/
 Description: Display posts, pages, custom post types, custom fields, files, images, comments, attachments, menus, or widget areas
-Version: 0.7.1
+Version: 0.8.8
 Author: Eliot Akira
 Author URI: eliotakira.com
 License: GPL2
+
+To do: display native read more tag
+
 */
 
 define('CCS_PATH', dirname(__FILE__));
@@ -18,6 +21,7 @@ global $sort_posts; global $sort_key;
 
 $ccs_global_variable = array(
 	'is_loop' => 'false',
+	'for_loop' => 'false',
 	'is_gallery_loop' => 'false',
 	'is_attachment_loop' => 'false',
 	'is_repeater_loop' => 'false',
@@ -58,9 +62,10 @@ require_once (CCS_PATH.'/includes/ccs-mobile.php'); 		// Mobile detect shortcode
 
 require_once (CCS_PATH.'/includes/ccs-content.php');		// Content shortcode
 require_once (CCS_PATH.'/includes/ccs-loop.php');			// Loop shortcode
+require_once (CCS_PATH.'/includes/ccs-foreach.php');		// For/Each shortcode
 require_once (CCS_PATH.'/includes/ccs-gallery.php');		// Simple gallery
 require_once (CCS_PATH.'/includes/ccs-bootstrap.php');		// Bootstrap support
-require_once (CCS_PATH.'/includes/ccs-field-loader.php');	// Load HTML, CSS, JS fields
+require_once (CCS_PATH.'/includes/ccs-load.php');			// Load HTML, CSS, JS fields
 require_once (CCS_PATH.'/includes/ccs-acf.php');			// Advanced Custom Fields support
 require_once (CCS_PATH.'/includes/ccs-user.php');			// Miscellaneous user shortcodes
 require_once (CCS_PATH.'/includes/ccs-docs.php');			// Documentation under Settings -> Content Shortcodes
