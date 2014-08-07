5d3621fa60434648e3767e529f5fb6a130ff7bff
miyarakira <miyarakira@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-08-07 04:12:36 +0000

    tagging 1.0.6
    git-svn-id: https://plugins.svn.wordpress.org/custom-content-shortcode/tags/1.0.6@961495 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ includes/ccs-if.php
@@ -0,0 +1,261 @@
+<?php
+
+
+class IfShortcode {
+
+	function __construct() {
+
+		global $ccs_global_variable;
+		$ccs_global_variable['if_flag'] = '';
+
+		add_action( 'init', array( &$this, 'register' ) );
+	}
+
+	function register() {
+		add_shortcode( 'if', array( &$this, 'if_shortcode' ) );
+		add_shortcode( 'flag', array( &$this, 'flag_shortcode' ) );
+	}
+
+	function if_shortcode( $atts, $content = null, $shortcode_name ) {
+
+		global $ccs_global_variable;
+
+		$args = array(
+			'every' => '',
+			'flag' => '',
+			'no_flag' => '',
+
+			'type' => '',
+			'name' => '',
+
+			'category' => '',
+			'tag' => '',
+			'taxonomy' => '',
+			'term' => '',
+			'compare' => 'OR',
+
+			'field' => '',
+			'value' => '',
+
+			'not' => '',
+		);
+
+		extract( shortcode_atts( $args , $atts, true ) );
+
+		if (is_array($atts)) $atts = array_flip($atts); /* To allow check for parameters with no value set */
+/*
+//		if ( (empty($flag))&&(empty($no_flag)) || (isset($atts['empty']))) return;
+		if ((isset($atts['empty'])) || (isset($atts['last'])) ) return; // [if empty] [if last] is processed by [loop]
+*/
+		if (!empty($no_flag)) $flag = $no_flag;
+		$compare = strtoupper($compare);
+
+		$out = '';
+		$condition = false;
+
+		/*========================================================================
+		 *
+		 * If we're inside loop shortcode
+		 *
+		 *=======================================================================*/
+
+		if ($ccs_global_variable['is_loop']=="true") {
+
+			if (!empty($every)) {
+
+				/*========================================================================
+				 *
+				 * Every X number of posts in [loop]
+				 *
+				 *=======================================================================*/
+
+				$count = $ccs_global_variable['current_loop_count'];
+
+				if (substr($every,0,4)=='not ') {
+					$every = substr($every, 4); // Remove first 4 letters
+
+					// not Modulo
+ 					$condition = ($every==0) ? false : (($count % $every)!=0);
+				} else {
+
+					// Modulo
+					$condition = ($every==0) ? false : (($count % $every)==0);
+				}
+
+				if (isset($atts['not'])) {
+					$condition = !$condition;
+				}
+
+				if ($condition) {
+					$out = do_shortcode( $content );
+				}
+
+				return $out;
+
+			}
+
+		} // End [loop] only conditions
+
+		/*========================================================================
+		 *
+		 * Get global post info
+		 *
+		 *=======================================================================*/
+
+		global $post;
+
+		if (empty($post)) return; // Make sure post exists
+
+		$current_post_type = isset($post->post_type) ? $post->post_type : null;
+		$current_post_name = isset($post->post_name) ? $post->post_name : null;
+		$current_post_id = isset($post->ID) ? $post->ID : null;
+
+		if (!empty($flag)) {
+
+			/*========================================================================
+			 *
+			 * Check field as condition [if flag="field"]
+			 *
+			 *=======================================================================*/
+
+			if ($ccs_global_variable['is_loop']=="true") {
+				$current_id = $ccs_global_variable['current_loop_id'];
+			} else {
+				$current_id = $current_post_id;
+			}
+			$check = get_post_meta( $current_id, $flag, true );
+
+			if ((!empty($check)) && (!empty($no_flag))) return;
+			if ((empty($check)) && (empty($no_flag))) return;
+			else {
+				$ccs_global_variable['if_flag'] = $check;
+				$out = do_shortcode( $content );
+				$ccs_global_variable['if_flag'] = '';
+				return $out;
+			}
+		}
+
+
+		/*========================================================================
+		 *
+		 * Taxonomy: category, tags, ..
+		 *
+		 *=======================================================================*/
+		
+		if (!empty($category)) {
+			$taxonomy = "category";
+			$term = $category;
+		}
+
+		if (!empty($tag)) {
+			$taxonomy = "post_tag";
+			$term = $tag;
+		}
+
+		if (!empty($taxonomy)) {
+
+			$taxonomies = wp_get_post_terms(
+				$current_post_id,
+				$taxonomy, array() );
+
+			$post_tax_array = array();
+			foreach ($taxonomies as $term_object) {
+				$post_tax_array[] = $term_object->slug;
+			}
+
+			$terms = self::comma_list_to_array($term);
+
+//			echo "Find $term in ";
+//			print_r($post_tax_array);
+
+			foreach ($terms as $term) {
+
+				if ($compare == "OR") {
+					$condition = in_array($term, $post_tax_array) ? true : $condition;
+				} else {
+					// AND
+					$condition = in_array($term, $post_tax_array) ? true : false;
+					if (!$condition) break; // Every term must be found
+				}
+
+			}
+
+		} // End taxonomy conditions
+
+
+		/*========================================================================
+		 *
+		 * Field: field="field_slug" value="this,that"
+		 *
+		 *=======================================================================*/
+		
+
+		if (!empty($field)) {
+
+			$check = get_post_meta( $current_post_id, $field, true );
+			if (!is_array($check)) $check = array($check);
+
+			$values = self::comma_list_to_array($value);
+
+			foreach ($values as $this_value) {
+
+				if ($compare == "OR") {
+					$condition = in_array($this_value, $check) ? true : $condition;
+				} else { // AND
+					$condition = in_array($this_value, $check) ? true : false;
+					if (!$condition) break; // Every term must be found
+				}
+			}
+		}		
+
+
+		/*========================================================================
+		 *
+		 * Post type, name
+		 *
+		 *=======================================================================*/
+
+		if (!empty($type)) {
+			$types = self::comma_list_to_array($types); // Enable comma-separated list
+			$condition = in_array($current_post_type, $types) ? true : false;
+		}
+
+		if (!empty($name)) {
+			$names = self::comma_list_to_array($name);
+			$condition = in_array($current_post_name, $names) ? true : false;
+		}
+
+		/*========================================================================
+		 *
+		 * Template: home, archive, single..
+		 * [if comment] - current post has comment
+		 *
+		 *=======================================================================*/
+		
+		$condition = isset($atts['home']) ? is_front_page() : $condition;
+		$condition = isset($atts['archive']) ? is_archive() : $condition;
+		$condition = isset($atts['single']) ? is_single() : $condition;
+		$condition = isset($atts['comment']) ? (get_comments_number($current_post_id)>0) : $condition;
+
+		$condition = isset($atts['not']) ? !$condition : $condition;
+
+		return $condition ? do_shortcode( $content ) : null;
+	}
+
+	function flag_shortcode() {
+		global $ccs_global_variable;
+		return $ccs_global_variable['if_flag'];
+	}
+
+
+	function comma_list_to_array( $string ) {
+
+		// Explode comma-separated list and trim white space
+
+		return array_map("trim", explode(",", $string));
+	}
+
+
+}
+new IfShortcode;
+
--- includes/ccs-user.php
+++ includes/ccs-user.php
@@ -1,198 +1,76 @@
 <?php
 
-
-
-
-/*====================================================================================================
+/*========================================================================
  *
- * Relative URL shortcodes - [url site/theme/child/content/uploads]
+ * Shortcodes:
+ * 
+ * user, is/isnt
+ * list_shortcodes, search_form, blog
  *
- *====================================================================================================*/
+ *=======================================================================*/
 
 
-class urlShortcode
-{
-    public static function userSettings()
-    {
-        $blogurl_settings = array();
+function custom_user_shortcode( $atts, $content ) {
 
-        $blogurl_settings['home'] = get_option( 'home' );
-        $blogurl_settings['wordpress'] = get_option( 'siteurl' );
-        $blogurl_settings['content'] = get_option( 'siteurl' ) . '/' . 'wp-content';
-        $blogurl_settings['templateurl'] = get_bloginfo( 'template_directory' );
-        $blogurl_settings['childtemplateurl'] = get_bloginfo( 'stylesheet_directory' );
-        
-        $blogurl_settings['insertslash'] = false;
-        
-        return $blogurl_settings;
-    }
-    
-    public static function custom_url( $attributes )
-    {
-        $blogurl_settings = urlShortcode::getSettings();
+	global $current_user;
+	get_currentuserinfo();
 
-		extract(shortcode_atts(array(
-			'login' => '',
-			'logout' => '',
-			'go' => '',
-		), $attributes));
+	extract(shortcode_atts(array(
+		'field' => '',
+		'meta' => ''
+	), $atts));
 
-        if( is_array( $attributes ) )
-        {
-            $attributes = array_flip( $attributes );
-        }
-        
+	if(!empty($meta))
+		$field=$meta;
 
-		if($go!='') {
-			if($go=='home')
-				$go = $blogurl_settings['home'];
-			elseif( (isset( $attributes['login'] )) || (isset( $attributes['logout'] )) )
-				if( !strpos ($go,"." ) )
-					$go = custom_content_shortcode(array('name'=>$go, 'field'=>'url'));
-		}
+	$out = null;
 
+	if (!empty($field)) {
+		return get_user_meta( $current_user->ID, $field, true );
+	}
 
-        if( isset( $attributes['wordpress'] ) )
-        {
-            $return_blogurl = $blogurl_settings['wordpress'];
-        }
-        elseif( isset( $attributes['uploads'] ) )
-        {
-            $return_blogurl = $blogurl_settings['uploads'];
-        }
-        elseif( isset( $attributes['content'] ) )
-        {
-            $return_blogurl = $blogurl_settings['content'];
-        }
-        elseif( isset( $attributes['layout'] ) )
-        {
-            $return_blogurl = $blogurl_settings['content'] . '/layout/';
-        }
-        elseif( isset( $attributes['theme'] ) )
-        {
-            $return_blogurl = $blogurl_settings['templateurl'];
-        }
-        elseif( isset( $attributes['child'] ) )
-        {
-            $return_blogurl = $blogurl_settings['childtemplateurl'];
-        }
-        elseif( isset( $attributes['login'] ) )
-        {
-        	$return_blogurl = wp_login_url( $go );
-        }
-        elseif( isset( $attributes['logout'] ) )
-        {
-			$return_blogurl = wp_logout_url( $go );
-        }
-        else
-        {
-            $return_blogurl = $blogurl_settings['home'];
-        }
-
-        if( isset( $attributes['slash'] ) || ( $blogurl_settings['insertslash'] && !isset( $attributes['noslash'] ) ) )
-        {
-            $return_blogurl .= '/';
-        }
-
-        return $return_blogurl;
-    }
-    
-    public static function getSettings()
-    {
-        $blogurl_settings = urlShortcode::userSettings();
-        $upload_dir = wp_upload_dir();
-        
-        if( !$upload_dir['error'] )
-        {
-            $blogurl_settings['uploads'] = $upload_dir['baseurl'];
-        }
-        elseif( '' != get_option( 'upload_url_path' ) )
-        {
-            // Prior to WordPress 3.5, this was set in Settings > Media > Full URL path to files
-            // In WordPress 3.5+ this is now hidden
-            $blogurl_settings['uploads'] = get_option( 'upload_url_path' );
-        }
-        else
-        {
-            $blogurl_settings['uploads'] = $blogurl_settings['wordpress'] . '/' . get_option( 'upload_path' );
-        }
-
-        return $blogurl_settings;
-    }
-}
-
-add_shortcode( 'url', array( 'urlShortcode', 'custom_url' ) );
-
-
-/*====================================================================================================
- *
- * Comment shortcodes - [comment form] form/template/count
- *
- *====================================================================================================*/
-
-
-function ccs_return_comment_form() {
-	ob_start();
-	comment_form( $args = array(
-		'id_form'           => 'commentform',  // that's the wordpress default value! delete it or edit it ;)
-		'id_submit'         => 'commentsubmit',
-		'title_reply'       => __( '' ),  // Leave a Reply - that's the wordpress default value! delete it or edit it ;)
-		'title_reply_to'    => __( '' ),  // Leave a Reply to %s - that's the wordpress default value! delete it or edit it ;)
-		'cancel_reply_link' => __( 'Cancel Reply' ),  // that's the wordpress default value! delete it or edit it ;)
-		'label_submit'      => __( 'Post Comment' ),  // that's the wordpress default value! delete it or edit it ;)
-			
-		'comment_field' =>  '<p><textarea placeholder="" id="comment" class="form-control" name="comment" cols="45" rows="8" aria-required="true"></textarea></p>', 
-			
-		'comment_notes_after' => ''
-	));
-	$form = ob_get_contents();
-    ob_end_clean();
-    return $form;
-}
-
-function ccs_return_comments_template($file) {
-	ob_start();
-	comments_template($file);
-	$form = ob_get_contents();
-    ob_end_clean();
-    return $form;
-}
-
-function custom_comment_shortcode( $atts, $content, $tag ) {
-
-	if( is_array( $atts ) ) {
+	if( is_array( $atts ) )
 		$atts = array_flip( $atts );
-	}
 
-	if( ($tag=='comments') || isset( $atts['template'] ) ) {
-		$content = ccs_return_comments_template($atts['template']);
-		return $content;
-	}
+	if( isset( $atts['name'] ) )
+		return $current_user->user_login;
 
-	if( isset( $atts['form'] ) ) {
-		$content = ccs_return_comment_form();
-		return $content;
-	}
-	if( isset( $atts['count'] ) ) {
-		return get_comments_number();
-	}
+	if( isset( $atts['id'] ) )
+		return $current_user->ID;
+
+	if( isset( $atts['email'] ) )
+		return $current_user->user_email;
+
+	if( isset( $atts['fullname'] ) )
+		return $current_user->display_name;
+
+	if( isset( $atts['avatar'] ) )
+		return get_avatar( $current_user->ID );
+
 }
-add_shortcode('comment', 'custom_comment_shortcode');
-add_shortcode('comments', 'custom_comment_shortcode');
+add_shortcode('user', 'custom_user_shortcode');
+
+
+/*========================================================================
+ *
+ * [is] shortcode - combine with [if]...
+ *
+ *=======================================================================*/
 
 function custom_is_shortcode( $atts, $content, $tag ) {
+
 	global $current_user;
 
 	extract(shortcode_atts(array(
 		'user' => '',
-		'format' => '',
-		'shortcode' => '',
+		'format' => 'false',
+		'shortcode' => 'true',
 	), $atts));
 
-	if($format == 'true') { // Format?
+	if ($format == 'true') { // Format?
 		$content = wpautop( $content );
 	}
-	if($shortcode != 'false') { // Shortcode?
+	if ($shortcode != 'false') { // Shortcode?
 		$content = do_shortcode( $content );
 	}
 
@@ -242,44 +120,25 @@ function custom_is_shortcode( $atts, $content, $tag ) {
 add_shortcode('is', 'custom_is_shortcode');
 add_shortcode('isnt', 'custom_is_shortcode');
 
-function custom_user_shortcode( $atts, $content ) {
+function custom_blog_shortcode( $atts, $content ){
 
-	global $current_user;
+	extract(shortcode_atts(array(
+		'id' => '',
+	), $atts));
 
-	get_currentuserinfo();
+	$out = $content;
 
-	if( is_array( $atts ) )
-		$atts = array_flip( $atts );
+	if ( empty($id) || !blog_exists($id))
+		return;
 
-	if( isset( $atts['name'] ) )
-		return $current_user->user_login;
+	switch_to_blog($id);
+	$out = do_shortcode($out);
+	restore_current_blog();
 
-	if( isset( $atts['id'] ) )
-		return $current_user->ID;
-
-	if( isset( $atts['email'] ) )
-		return $current_user->user_email;
-
-	if( isset( $atts['fullname'] ) )
-		return $current_user->display_name;
-
-	if( isset( $atts['avatar'] ) )
-		return get_avatar( $current_user->ID );
-
+	return $out;
 }
-add_shortcode('user', 'custom_user_shortcode');
+add_shortcode('blog', 'custom_blog_shortcode');
 
-
-function custom_br_shortcode( $atts, $content ) {
-	return '<br>';
-}
-add_shortcode('br', 'custom_br_shortcode');
-
-
-function custom_p_shortcode( $atts, $content ) {
-	return '<p>' . $content . '</p>';
-}
-add_shortcode('p', 'custom_p_shortcode');
 
 
 function custom_list_shortcodes( ) {
@@ -312,3 +171,48 @@ function custom_search_form_shortcode() {
 }
 add_shortcode('search_form', 'custom_search_form_shortcode');
 
+if ( ! function_exists( 'blog_exists' ) ) {
+
+    /**
+     * Checks if a blog exists and is not marked as deleted.
+     *
+     * @link   http://wordpress.stackexchange.com/q/138300/73
+     * @param  int $blog_id
+     * @param  int $site_id
+     * @return bool
+     */
+    function blog_exists( $blog_id, $site_id = 0 ) {
+
+        global $wpdb;
+        static $cache = array ();
+
+        $site_id = (int) $site_id;
+
+       	if (!function_exists('get_current_site'))
+       		return false;
+
+        if ( 0 === $site_id ) {
+        	$current_site = get_current_site();
+            $site_id = $current_site->id;
+        }
+
+        if ( empty ( $cache ) or empty ( $cache[ $site_id ] ) ) {
+
+            if ( wp_is_large_network() ) // we do not test large sites.
+                return TRUE;
+
+            $query = "SELECT `blog_id` FROM $wpdb->blogs
+                    WHERE site_id = $site_id AND deleted = 0";
+
+            $result = $wpdb->get_col( $query );
+
+            // Make sure the array is always filled with something.
+            if ( empty ( $result ) )
+                $cache[ $site_id ] = array ( 'do not check again' );
+            else
+                $cache[ $site_id ] = $result;
+        }
+
+        return in_array( $blog_id, $cache[ $site_id ] );
+    }
+}
