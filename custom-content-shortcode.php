7b8ac73130550f386ed266a31643446838d1d06a
miyarakira <miyarakira@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-08-24 22:04:28 +0000

    tagging 1.1.2
    git-svn-id: https://plugins.svn.wordpress.org/custom-content-shortcode/tags/1.1.2@971900 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- custom-content-shortcode.php
+++ custom-content-shortcode.php
@@ -3,7 +3,7 @@
 Plugin Name: Custom Content Shortcode
 Plugin URI: http://wordpress.org/plugins/custom-content-shortcode/
 Description: Display posts, pages, custom post types, custom fields, files, images, comments, attachments, menus, or widget areas
-Version: 0.7.1
+Version: 1.1.2
 Author: Eliot Akira
 Author URI: eliotakira.com
 License: GPL2
@@ -12,57 +12,65 @@ License: GPL2
 define('CCS_PATH', dirname(__FILE__));
 define('CCS_URL', untrailingslashit(plugins_url('/',__FILE__)));
 
-$ccs_content_template_loader = false; // Set true to enable template loader
-
-global $sort_posts; global $sort_key;
-
-$ccs_global_variable = array(
-	'is_loop' => 'false',
-	'is_gallery_loop' => 'false',
-	'is_attachment_loop' => 'false',
-	'is_repeater_loop' => 'false',
-	'is_acf_gallery_loop' => 'false',
-	'current_loop_id' => '',
-	'current_row' => '',
-	'current_image' => '',
-	'current_image_url' => '',
-	'current_image_thumb' => '',
-	'current_image_thumb_url' => '',
-	'current_image_caption' => '',
-	'current_image_title' => '',
-	'current_image_description' => '',
-	'current_image_alt' => '',
-	'current_image_ids' => '',
-	'current_gallery_name' => '',
-	'current_gallery_id' => '',
-	'current_attachment_id' => '',
-	'current_attachment_ids' => '',
-	'current_script' => '',
-);
-
-
-/**
- * Set up mobile detect library
- *
- */
-
-if (!class_exists('Mobile_Detect')) {
-	require_once (CCS_PATH.'/includes/Mobile_Detect.php');	
-}
-
-$detect = new Mobile_Detect();
-$device_type = ($detect->isMobile() ? ($detect->isTablet() ? 'tablet' : 'phone') : 'computer');
-
-require_once (CCS_PATH.'/includes/ccs-mobile.php'); 		// Mobile detect shortcodes
-
-
+require_once (CCS_PATH.'/includes/ccs-core.php');			// Core functions
 require_once (CCS_PATH.'/includes/ccs-content.php');		// Content shortcode
 require_once (CCS_PATH.'/includes/ccs-loop.php');			// Loop shortcode
-require_once (CCS_PATH.'/includes/ccs-gallery.php');		// Simple gallery
-require_once (CCS_PATH.'/includes/ccs-bootstrap.php');		// Bootstrap support
-require_once (CCS_PATH.'/includes/ccs-field-loader.php');	// Load HTML, CSS, JS fields
-require_once (CCS_PATH.'/includes/ccs-acf.php');			// Advanced Custom Fields support
-require_once (CCS_PATH.'/includes/ccs-user.php');			// Miscellaneous user shortcodes
-require_once (CCS_PATH.'/includes/ccs-docs.php');			// Documentation under Settings -> Content Shortcodes
+require_once (CCS_PATH.'/includes/ccs-loop-attached.php');	// Attachment loop
 
+require_once (CCS_PATH.'/includes/ccs-comments.php');		// Comments shortcode
+require_once (CCS_PATH.'/includes/ccs-user.php');			// User shortcodes
+require_once (CCS_PATH.'/includes/ccs-url.php');			// URL shortcode
+
+require_once (CCS_PATH.'/includes/ccs-foreach.php');		// For/Each shortcode
+require_once (CCS_PATH.'/includes/ccs-if.php');				// If shortcode
+
+require_once (CCS_PATH.'/includes/ccs-docs.php');			// Documentation under Settings -> Custom Content
+
+// Future update
+
+// Core needs patch to prevent PHP notice for get_widget()
+// require_once (CCS_PATH.'/includes/ccs-widget.php');			// Widget shortcode
+
+
+/*========================================================================
+ *
+ * Optional modules
+ *
+ *=======================================================================*/
+
+$settings = get_option('ccs_content_settings');
+
+if ($settings === false ) {
+	// Default settings
+
+	$settings['move_wpautop'] = 'off';
+	$settings['load_acf_module'] = 'on';
+	$settings['load_bootstrap_module'] = 'on';
+	$settings['load_file_loader'] = 'on';
+	$settings['load_gallery_field'] = 'on';
+	$settings['load_mobile_detect'] = 'on';
+	update_option( 'ccs_content_settings', $settings );
+}
+
+$load_acf_module = isset( $settings['load_acf_module'] ) ?
+	$settings['load_acf_module'] : 'off';
+$load_bootstrap_module = isset( $settings['load_bootstrap_module'] ) ?
+	$settings['load_bootstrap_module'] : 'off';
+$load_file_loader = isset( $settings['load_file_loader'] ) ?
+	$settings['load_file_loader'] : 'off';
+$load_gallery_field = isset( $settings['load_gallery_field'] ) ?
+	$settings['load_gallery_field'] : 'off';
+$load_mobile_detect = isset( $settings['load_mobile_detect'] ) ?
+	$settings['load_mobile_detect'] : 'off';
+
+if ($load_gallery_field == "on")
+	require_once (CCS_PATH.'/includes/ccs-gallery.php');		// Simple gallery
+if ($load_bootstrap_module == "on")
+	require_once (CCS_PATH.'/includes/ccs-bootstrap.php');		// Bootstrap support
+if ($load_acf_module == "on")
+	require_once (CCS_PATH.'/includes/ccs-acf.php');			// Advanced Custom Fields support
+if ($load_file_loader == "on")
+	require_once (CCS_PATH.'/includes/ccs-load.php');			// Load HTML, CSS, JS fields
+if ($load_mobile_detect == "on")
+	require_once (CCS_PATH.'/includes/ccs-mobile.php'); 		// Mobile detect shortcodes
 
