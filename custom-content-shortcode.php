7335ac41b3a5fcc7e9ed4249898771a6d359f189
miyarakira <miyarakira@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-10-04 04:10:15 +0000

    tagging 1.4.1
    git-svn-id: https://plugins.svn.wordpress.org/custom-content-shortcode/tags/1.4.1@1001578 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- custom-content-shortcode.php
+++ custom-content-shortcode.php
@@ -3,7 +3,8 @@
 Plugin Name: Custom Content Shortcode
 Plugin URI: http://wordpress.org/plugins/custom-content-shortcode/
 Description: Display posts, pages, custom post types, custom fields, files, images, comments, attachments, menus, or widget areas
-Version: 0.7.1
+Version: 1.4.1
+Shortcodes: loop, content, field, taxonomy, if, for, each, comments, user, url, load...
 Author: Eliot Akira
 Author URI: eliotakira.com
 License: GPL2
@@ -12,57 +13,197 @@ License: GPL2
 define('CCS_PATH', dirname(__FILE__));
 define('CCS_URL', untrailingslashit(plugins_url('/',__FILE__)));
 
-$ccs_content_template_loader = false; // Set true to enable template loader
+new CCS_Plugin;
 
-global $sort_posts; global $sort_key;
+class CCS_Plugin {
 
-$ccs_global_variable = array(
-	'is_loop' => 'false',
-	'is_gallery_loop' => 'false',
-	'is_attachment_loop' => 'false',
-	'is_repeater_loop' => 'false',
-	'is_acf_gallery_loop' => 'false',
-	'current_loop_id' => '',
-	'current_row' => '',
-	'current_image' => '',
-	'current_image_url' => '',
-	'current_image_thumb' => '',
-	'current_image_thumb_url' => '',
-	'current_image_caption' => '',
-	'current_image_title' => '',
-	'current_image_description' => '',
-	'current_image_alt' => '',
-	'current_image_ids' => '',
-	'current_gallery_name' => '',
-	'current_gallery_id' => '',
-	'current_attachment_id' => '',
-	'current_attachment_ids' => '',
-	'current_script' => '',
-);
+	public static $settings;
+
+	function __construct() {
+
+		$this->load_settings();
+		$this->load_main_modules();
+		$this->load_optional_modules();
+		$this->setup_wp_filters();
+	}
 
 
-/**
- * Set up mobile detect library
- *
- */
+	/*========================================================================
+	 *
+	 * Load settings
+	 *
+	 *=======================================================================*/
 
-if (!class_exists('Mobile_Detect')) {
-	require_once (CCS_PATH.'/includes/Mobile_Detect.php');	
+	function load_settings() {
+
+		$option_key = 'ccs_content_settings';
+
+		self::$settings = get_option( $option_key );
+
+		if ( self::$settings === false ) {
+
+			// Default settings
+
+			self::$settings['load_acf_module'] = 'on';
+			self::$settings['load_bootstrap_module'] = 'on';
+			self::$settings['load_file_loader'] = 'on';
+			self::$settings['load_gallery_field'] = 'on';
+
+			self::$settings['load_mobile_detect'] = 'off';
+
+			self::$settings['shortcodes_in_widget'] = 'on';
+
+			self::$settings['raw_shortcode'] = 'off';
+			self::$settings['move_wpautop'] = 'off';
+			self::$settings['shortcode_unautop'] = 'off';
+
+			update_option( $option_key, self::$settings );
+		}
+	}
+
+
+	/*========================================================================
+	 *
+	 * Load main and optional modules
+	 *
+	 *=======================================================================*/
+
+	function load_module( $module ) {
+
+		require_once (CCS_PATH.'/includes/'.$module.'.php');
+
+	}
+
+	function load_main_modules() {
+
+		$main_modules = array(
+			'content',		// Content shortcode
+			'loop',			// Loop shortcode
+			'attached',		// Attachment loop
+			'comments',		// Comments shortcode
+			'user',			// User shortcodes
+			'url',			// URL shortcode
+			'foreach',		// For/each loop
+			'if',			// If shortcode
+			'docs',			// Documentation under Settings -> Custom Content
+			'cache',		// Cache shortcode
+			'format',		// Format shortcodes: br, p, x, clean, direct, format
+			'wck',			// WCK support
+		//	'widget'		// Widget shortcode (not ready)
+		);
+
+		foreach ($main_modules as $module) {
+
+			$this->load_module( $module );
+		}
+	}
+
+	/*========================================================================
+	 *
+	 * Optional modules
+	 *
+	 *=======================================================================*/
+
+	function load_optional_modules() {
+
+		$optional_modules = array(
+
+			// Option name => module name
+
+			'load_gallery_field'	=> 'gallery',		// Gallery field
+			'load_acf_module'		=> 'acf',			// Advanced Custom Fields support
+			'load_file_loader'		=> 'load',			// Load HTML, CSS, JS fields
+			'load_mobile_detect'	=> 'mobile',		// Mobile detect shortcodes
+			'load_bootstrap_module'	=> 'bootstrap',		// Bootstrap support
+			'shortcode_unautop'		=> 'unautop',		// Shortcode unautop
+			'raw_shortcode'			=> 'raw',			// [raw]
+		);
+
+		foreach ($optional_modules as $option => $module) {
+
+			if ( isset(self::$settings[ $option ]) && self::$settings[ $option ]=='on' ) {
+
+				$this->load_module( $module );
+			}
+		}
+	}
+
+
+	/*========================================================================
+	 *
+	 * Set up WP filters
+	 *
+	 *=======================================================================*/
+	
+	function setup_wp_filters() {
+
+		$settings = self::$settings;
+
+		/*========================================================================
+		 *
+		 * Enable shortcodes in widget
+		 *
+		 *=======================================================================*/
+
+		if ( isset( $settings['shortcodes_in_widget'] ) &&
+			($settings['shortcodes_in_widget'] == "on") ) {
+				
+			add_filter('widget_text', 'do_shortcode');
+		}
+
+		// Exempt [loop] from wptexturize()
+		add_filter( 'no_texturize_shortcodes', array( $this, 'shortcodes_to_exempt_from_wptexturize') );
+
+
+		/*========================================================================
+		 *
+		 * Move wpautop filter to after shortcode processing (legacy)
+		 * 
+		 * User feedback suggests some themes/plugins don't work well with the
+		 * filter moved, because they assume default priority. Instead, use [raw]
+		 * or edit code outside of post editor.
+		 *
+		 *=======================================================================*/
+
+		if ( isset( $settings['move_wpautop'] ) &&
+			($settings['move_wpautop'] == "on") ) {
+
+			remove_filter( 'the_content', 'wpautop' );
+			add_filter( 'the_content', 'wpautop' , 99);
+			add_filter( 'the_content', 'shortcode_unautop',100 );
+		}
+
+	}
+
+	function shortcodes_to_exempt_from_wptexturize($shortcodes){
+		$shortcodes[] = 'loop';
+		return $shortcodes;
+	}
+	
 }
 
-$detect = new Mobile_Detect();
-$device_type = ($detect->isMobile() ? ($detect->isTablet() ? 'tablet' : 'phone') : 'computer');
 
-require_once (CCS_PATH.'/includes/ccs-mobile.php'); 		// Mobile detect shortcodes
+/*========================================================================
+ *
+ * Global helper functions
+ *
+ *=======================================================================*/
 
+if (!function_exists('do_short')) {
+	function do_short($content) {
+		echo do_shortcode($content);
+	}
+}
 
-require_once (CCS_PATH.'/includes/ccs-content.php');		// Content shortcode
-require_once (CCS_PATH.'/includes/ccs-loop.php');			// Loop shortcode
-require_once (CCS_PATH.'/includes/ccs-gallery.php');		// Simple gallery
-require_once (CCS_PATH.'/includes/ccs-bootstrap.php');		// Bootstrap support
-require_once (CCS_PATH.'/includes/ccs-field-loader.php');	// Load HTML, CSS, JS fields
-require_once (CCS_PATH.'/includes/ccs-acf.php');			// Advanced Custom Fields support
-require_once (CCS_PATH.'/includes/ccs-user.php');			// Miscellaneous user shortcodes
-require_once (CCS_PATH.'/includes/ccs-docs.php');			// Documentation under Settings -> Content Shortcodes
-
+if (!function_exists('start_short')) {
+	function start_short() {
+		ob_start();
+	}
+}
+if (!function_exists('end_short')) {
+	function end_short() {
+		$out = ob_get_clean();
+		do_short($out);
+	}
+}
 
