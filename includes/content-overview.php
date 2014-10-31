9d9f37ccdf2f6478ca78ddfd80468cffc32a5c98
miyarakira <miyarakira@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-10-31 11:18:11 +0000

    tagging 1.4.4
    git-svn-id: https://plugins.svn.wordpress.org/custom-content-shortcode/tags/1.4.4@1017289 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- includes/content-overview.php
+++ includes/content-overview.php
@@ -1,36 +1,42 @@
 
-<div style="max-width:960px;">
+<div id="content-overview-page">
 
-	<h2  style="padding-left:10px">Content Overview</h2>
-
-	<div style="height:10px"></div>
+	<h2>Content Overview</h2>
 
 	<hr>
 
-	<h3 style="padding-left:10px">Post types and fields</h3>
-	<div style="height:10px"></div>
+<?php
+
+/*========================================================================
+ *
+ * Post types and fields
+ *
+ *=======================================================================*/
+
+?>
+	<h3>Post types and fields</h3>
 
 	<table class="wp-list-table widefat fixed posts">
 		<thead>
 			<tr>
-				<th><b>Post type</b></th>
-				<th><b></b></th>
-				<th><b>Taxonomy</b></th>
-				<th><b></b></th>
-				<th><b>Default</b></th>
-				<th><b>Fields</b></th>
-				<th class="column-author"><b>Count</b></th>
+				<th width="12%">Post type</th>
+				<th width="12%"></th>
+				<th width="12%">Taxonomy</th>
+				<th width="12%"></th>
+				<th width="15%">Fields</th>
+				<th>Default</th>
+				<th width="7%">Count</th>
 			</tr>
 		</thead>
 		<tfoot>
 			<tr>
-				<th><b>Post type</b></th>
-				<th><b></b></th>
-				<th><b>Taxonomy</b></th>
-				<th><b></b></th>
-				<th><b>Default</b></th>
-				<th><b>Fields</b></th>
-				<th class="column-author"><b>Count</b></th>
+				<th>Post type</th>
+				<th></th>
+				<th>Taxonomy</th>
+				<th></th>
+				<th>Fields</th>
+				<th>Default</th>
+				<th>Count</th>
 			</tr>
 		</tfoot>
 		<tbody id="the-list">
@@ -86,7 +92,11 @@
 				}
 
 				$all_supports = array();
-				$support_types = array('title', 'author', 'thumbnail', 'excerpt');
+				$support_types = array(
+					'title',
+					'author',
+					'thumbnail',
+					'excerpt');
 
 				foreach ($support_types as $support_type) {
 					if (post_type_supports($post_type, $support_type))
@@ -100,7 +110,7 @@
 					<tr <?php echo $alternate; ?>>
 
 
-						<td style="vertical-align:top;" class="column-title">
+						<td class="column-title">
 
 							<?php
 
@@ -137,7 +147,7 @@
 
 						</td>
 
-						<td style="vertical-align:top">
+						<td>
 
 							<?php  echo $post_type . '<br>'; ?>
 
@@ -208,7 +218,7 @@
 
 
 
-		<td style="vertical-align:top">
+		<td>
 				<?php
 			        $taxonomies = get_object_taxonomies($post_type);
 
@@ -222,7 +232,7 @@
 					/*		echo implode(', ', $taxonomies ); */
 				?>
 		</td>
-		<td style="vertical-align:top">
+		<td>
 				<?php
 			        foreach ($taxonomies as $row => $taxonomy) {
 
@@ -236,20 +246,39 @@
 
 
 
-		<td style="vertical-align:top">
+		<td>
+			<?php
+				if ( empty( $all_fields) ) {
+					echo '<br>'; // Prevent cell from collapsing
+				} else {
 
+					ksort( $all_fields );
+
+					foreach ( $all_fields as $key => $value ) {
+						echo $key . '<br>';
+					}
+				}
+/*
+			echo implode(', ', array_keys($all_fields) );
+*/			?>
+		</td>
+
+		<td>
 		<?php
+
+// Default fields
+
 
 			$default_supports = array('id', 'date', 'url', 'slug', );
 
 			$all_supports = array_merge($default_supports, $all_supports);
-
+/*
 			if (in_array('author', $all_supports)) {
-				$add_supports = array('avatar'); 
-/*				$add_supports = array('author-id', 'author-url', 'avatar'); */
+//				$add_supports = array('avatar'); 
+//				$add_supports = array('author-id', 'author-url', 'avatar');
 				$all_supports = array_merge($add_supports, $all_supports);
 			}
-
+*/
 			if (in_array('thumbnail', $all_supports)) {
 				$add_supports = array('image'); 
 /*				$add_supports = array('image', 'image-url', 'thumbnail-url'); */
@@ -272,27 +301,8 @@
 
 
 		</td>
-		<td style="vertical-align:top">
-			<?php
-				if ( empty( $all_fields) ) {
-					echo '<br>'; // Prevent cell from collapsing
-				} else {
 
-					ksort( $all_fields );
-
-					foreach ( $all_fields as $key => $value ) {
-						echo $key . '<br>';
-					}
-				}
-/*
-			echo implode(', ', array_keys($all_fields) );
-*/			?>
-		
-		</td>
-
-
-
-		<td style="vertical-align:top;" class="column-author">
+		<td class="column-author">
 
 		<?php
 			echo $post_count[ $post_type ] . '<br>';
@@ -314,27 +324,33 @@
 	</tbody>
 	</table>
 
-	<div style="height:40px"></div>
 	<hr>
-	<h3 style="padding-left:10px">Taxonomies</h3>
-	<div style="height:10px"></div>
+<?php
 
-	<div style="max-width:960px;">
+/*========================================================================
+ *
+ * Taxonomies
+ *
+ *=======================================================================*/
+
+?>
+	<h3>Taxonomies</h3>
+
 	<table class="wp-list-table widefat fixed posts">
 		<thead>
 			<tr>
-				<th><b>Taxonomy</b></th>
-				<th><b></b></th>
-				<th><b>Terms</b></th>
-				<th><b></b></th>
+				<th width="15%">Taxonomy</th>
+				<th width="35%"></th>
+				<th>Terms</th>
+				<th></th>
 			</tr>
 		</thead>
 		<tfoot>
 			<tr>
-				<th><b>Taxonomy</b></th>
-				<th><b></b></th>
-				<th><b>Terms</b></th>
-				<th><b></b></th>
+				<th>Taxonomy</th>
+				<th></th>
+				<th>Terms</th>
+				<th></th>
 			</tr>
 		</tfoot>
 		<tbody id="the-list">
@@ -350,7 +366,7 @@
 
 			        foreach ($taxonomies as $row => $taxonomy) {
 
-			        	if ( ! in_array($taxonomy, $done) ) {	// Duplicate?
+			        	if (( !in_array($taxonomy, $done) ) && ($taxonomy!="post_format") ) { // Duplicate?
 
 			        	$done[] = $taxonomy;
 						$alternate = ( $alternate == '' ) ? 'class="alternate"' : '';
@@ -358,40 +374,43 @@
 						?>
 						<tr <?php echo $alternate; ?>>
 
-							<td style="vertical-align:top">
+							<td class="row-title">
 
 								<?php
 
-						        	$the_tax = get_taxonomy( $taxonomy );
+						        	$the_tax = get_taxonomy( $taxonomy, 'hide_empty=0' );
 
-									echo '<a class="row-title" href="' . admin_url( 'edit-tags.php?taxonomy=' . $taxonomy ) . '">';
-									echo $the_tax->labels->name . '</a><br>';
-
+						        	if (($taxonomy=="category")||($taxonomy=="post_tag")) {
+										echo '<a href="' . admin_url( 'edit-tags.php?taxonomy=' . $taxonomy ) . '">';
+										echo $the_tax->labels->name . '</a><br>';
+						        	} else {
+										echo $the_tax->labels->name . '<br>';
+						        	}
 								?>
 
 							</td>
-							<td style="vertical-align:top">
+							<td>
 
 								<?php
 
-						        	$the_tax = get_taxonomy( $taxonomy );
+//						        	$the_tax = get_taxonomy( $taxonomy, 'hide_empty=0' );
 
 									echo $taxonomy . '<br>';
 
 								?>
 
 							</td>
-							<td style="vertical-align:top">
+							<td>
 								<?php
 
-								$terms = get_terms( $taxonomy );
+								$terms = get_terms( $taxonomy, array('hide_empty'=>0) ); // Show empty terms
 
 								foreach ( $terms as $term ) {
 									echo $term->name . '<br>';
 								}
 								?>
 							</td>
-							<td style="vertical-align:top">
+							<td>
 								<?php
 
 								foreach ( $terms as $term ) {
@@ -413,30 +432,192 @@
 
 		</tbody>
 	</table>
-	</div>
 
-	<div style="height:40px"></div>
+	<a href="#" id="user-roles"></a>
 	<hr>
-	<h3 style="padding-left:10px">Registered shortcodes</h3>
-	<div style="height:10px"></div>
+<?php
 
-	<div style="max-width:960px;">
+/*========================================================================
+ *
+ * User meta
+ *
+ *=======================================================================*/
+
+?>
+	<h3 class="dropdown-action" data-id="user-meta-table">User meta</h3>
+
+	<div id="user-meta-table" class="dropdown-body">
 
 	<table class="wp-list-table widefat fixed posts">
 		<thead>
 			<tr>
-				<th><b>Shortcode</b></th>
-				<th><b>Function</b></th>
-				<th><b>Shortcode</b></th>
-				<th><b>Function</b></th>
+				<th>Fields</th>
+				<th></th>
+				<th></th>
+				<th></th>
+			</tr>
+		</thead>
+		<tbody id="the-list">
+
+			<?php
+
+			// Show all user fields
+
+			$all_meta = get_user_meta( 1 );
+			ksort($all_meta);
+
+			echo '<tr>';
+			$count = 0;
+			$max = count($all_meta);
+			$break = round($max / 4);
+
+			if ($break==0) echo '<td>';
+
+			foreach ($all_meta as $key => $value) {
+
+				$count++;
+
+				if ( ($break>0) && ($count % $break == 1) ) echo '<td>';
+				echo $key.'<br>';
+				if ( ($break>0) && ($count % $break == 0) ) echo '</td>';
+			}
+
+			if ( ($break==0) || ($count % $break != 0)) echo '</td>';
+			echo '</tr>';
+
+			?>
+		</tbody>
+	</table>
+	</div>
+
+	<hr>
+<?php
+
+/*========================================================================
+ *
+ * User roles and capabilities
+ *
+ *=======================================================================*/
+
+?>
+	<h3 class="dropdown-action" data-id="user-roles-table">User roles</h3>
+
+	<div id="user-roles-table" class="dropdown-body">
+
+	<table class="wp-list-table widefat fixed posts">
+		<thead>
+			<tr>
+				<th width="15%">Role</th>
+				<th width="15%"></th>
+				<th>Capabilities</th>
 			</tr>
 		</thead>
 		<tfoot>
 			<tr>
-				<th><b>Shortcode</b></th>
-				<th><b>Function</b></th>
-				<th><b>Shortcode</b></th>
-				<th><b>Function</b></th>
+				<th>Role</th>
+				<th></th>
+				<th>Capabilities</th>
+			</tr>
+		</tfoot>
+		<tbody id="the-list">
+
+				<?php
+
+				// Show all roles
+
+				global $wp_roles;
+				 
+				// get a role based on role name, does the same thing as get_role()
+
+				$roles = $wp_roles->roles;
+/*
+				echo '<pre>';
+				print_r($wp_roles);
+				echo '</pre>';
+*/
+//				ksort($roles); // Alphabetical sort
+
+
+				$row_num = 0;
+				$alternate = '';
+
+				foreach ( $roles as $role_slug => $role ) {
+
+					if ($row_num % 2 == 0) {
+						$alternate = ( $alternate == '' ) ? 'class="alternate"' : '';
+					}
+					echo '<tr ' . $alternate . '>';
+
+					?>
+						<td class="row-title">
+								<?php
+									echo $role['name'] . '<br>';
+								?>
+						</td>
+						<td class="text-left">
+								<?php
+									echo $role_slug . '<br>';
+								?>
+						</td>
+
+						<td>
+							<?php
+
+							$capabilities = array();
+							$capabilities_list = $role['capabilities'];
+							ksort($capabilities_list);
+
+							foreach ($capabilities_list as $capability => $value) {
+								$capabilities[] = $capability;
+							}
+
+							echo implode(", ", $capabilities) . '<br>';
+
+							?>
+						</td>
+
+					<?php
+
+					echo '</tr>';
+
+					$row_num++;
+				}	// Each shortcode
+
+				?>
+
+		</tbody>
+	</table>
+	</div>
+
+	<hr>
+<?php
+
+/*========================================================================
+ *
+ * Registered shortcodes
+ *
+ *=======================================================================*/
+
+?>
+	<h3 class="dropdown-action" data-id="registered-shortcodes-table">Registered shortcodes</h3>
+
+	<div id="registered-shortcodes-table" class="dropdown-body">
+
+	<table class="wp-list-table widefat fixed posts">
+		<thead>
+			<tr>
+				<th>Shortcode</th>
+				<th>Function</th>
+				<th>Shortcode</th>
+				<th>Function</th>
+			</tr>
+		</thead>
+		<tfoot>
+			<tr>
+				<th>Shortcode</th>
+				<th>Function</th>
+				<th>Shortcode</th>
+				<th>Function</th>
 			</tr>
 		</tfoot>
 		<tbody id="the-list">
@@ -459,9 +640,9 @@
 					}
 
 					?>
-						<td style="vertical-align:top">
+						<td style="vertical-align:top" class="row-title">
 								<?php
-									echo '<a class="row-title">[' . $key . ']</a><br>';
+									echo '[' . $key . ']<br>';
 								?>
 						</td>
 
@@ -515,9 +696,30 @@
 		</tbody>
 	</table>
 	</div>
-	<div style="height:40px"></div>
+<hr>
 
-	<div style="padding-left:5px;">
-		<a href="options-general.php?page=ccs_content_shortcode_help"><em>Reference: Custom Content Shortcode</em></a>
+	<div style="margin:20px 0 0 5px;">
+		<a href="options-general.php?page=ccs_reference"><em>Reference: Custom Content Shortcode</em></a>
 	</div>
 </div>
+<script type="text/javascript">
+	
+// Dropdown action
+
+jQuery(document).ready(function($){
+
+	$('.dropdown-action').on('click',function(){
+		var dropID = $(this).data('id');
+		$('#'+dropID).slideToggle('fast');
+
+		var $toggled = $(this).children('.toggled');
+
+		if ($toggled.html()==' + ')
+			$toggled.html(' - ');
+		else if ($toggled.html()==' - ')
+			$toggled.html(' + ');
+	});
+
+});
+
+</script>
