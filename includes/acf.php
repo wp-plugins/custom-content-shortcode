313540c3f4b538635aed094feba79eb067388a46
miyarakira <miyarakira@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-11-07 09:21:51 +0000

    tagging 1.4.8
    git-svn-id: https://plugins.svn.wordpress.org/custom-content-shortcode/tags/1.4.8@1021409 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ includes/acf.php
@@ -0,0 +1,360 @@
+<?php
+
+/*====================================================================================================
+ *
+ * Shortcodes for Advanced Custom Fields
+ * 
+ * Gallery, repeater, flexible content, relationship/post object
+ * 
+ *====================================================================================================*/
+
+new CCS_To_ACF;
+
+class CCS_To_ACF {
+
+	public static $state;
+
+	function __construct() {
+
+		self::$state['is_relationship_loop'] = 'false';
+		self::$state['is_repeater_or_flex_loop'] = 'false';
+
+		if (!function_exists('get_field')) return; // If ACF is not installed
+
+		add_shortcode('sub', array($this, 'sub_field'));
+		add_shortcode('flex', array($this, 'loop_through_acf_field'));
+
+		// This will be called by [repeater] if not inside WCK metabox
+//		add_shortcode('repeater', array($this, 'loop_through_acf_field'));
+		add_shortcode('-repeater', array($this, 'loop_through_acf_field')); // Nested repeater
+
+		add_shortcode('acf_gallery', array($this, 'loop_through_acf_gallery_field'));
+		add_shortcode('acf_image', array($this, 'get_image_details_from_acf_gallery'));
+		add_shortcode('sub_image', array($this, 'get_image_details_from_acf_gallery')); // Alias
+		add_shortcode('layout', array($this, 'if_get_row_layout'));
+
+		// This will be called by [related] when relationship field is specified
+//		add_shortcode('related', array($this, 'loop_relationship_field'));
+
+		// Legacy - to be removed in a future update
+		add_shortcode('live-edit', array($this, 'call_live_edit'));
+	}
+
+	public static function sub_field( $atts ) {
+
+		extract(shortcode_atts(array(
+			'field' => '',
+			'format' => '',
+			'image' => '',
+			'in' => '',
+			'size' => '',
+		), $atts));
+
+		if ($image!='') {
+
+			$output = get_sub_field($image);
+
+			if ( $output != '' ) {
+
+				if ($size=='') $size='full';
+
+				switch($in) {
+					case 'id' : $output = wp_get_attachment_image( $output, $size ); break;
+					case 'url' : $output = '<img src="' . $output . '">'; break;
+					default : /* image object */
+						if (is_array($output)) {
+							$output = wp_get_attachment_image( $output['id'], $size );
+						} else {
+							$output = wp_get_attachment_image( $output, $size ); // Assume it's ID
+						}
+				}
+			}
+
+		} else {
+
+			$output = get_sub_field($field);
+
+			if ( ($format=='true') && ($output!='') ) {
+				$output = wpautop($output);
+			}
+		}
+		return $output;
+	}
+
+	public static function loop_through_acf_field( $atts, $content ) {
+
+		/* For repeater and flexible content fields */
+
+		extract( shortcode_atts( array(
+			'field' => '',
+			'count' => '',
+			'start' => '',
+			'columns' => '', 'pad' => '', 'between' => '', 
+		), $atts ));
+
+		if ( have_rows( $field )) {
+
+			$index_now = 0;
+			$outputs = array();
+
+			if ( $start == '' ) $start='1';
+
+			while ( have_rows( $field ) ) {
+
+				self::$state['is_repeater_or_flex_loop'] = 'true'; // Keep true for each row in case nested
+
+				the_row(); // Move index forward
+
+				$index_now++;
+
+				if ( $index_now >= $start ) { /* Start loop */
+
+					if ( ( !empty($count) ) && ( $index_now >= ($start+$count) ) ) {
+							/* If over count, continue empty looping for has_sub_field */
+					} else {
+
+						$outputs[] = str_replace( '{COUNT}', $index_now, do_shortcode($content) );
+
+					}
+				}
+			}
+
+			self::$state['is_repeater_or_flex_loop'] = 'false';
+
+		} else {
+			return null;
+		}
+
+		if( !empty($outputs) && is_array($outputs)) {
+
+			if (!empty($columns)) {
+
+				$output = CCS_Loop::render_columns( $outputs, $columns, $pad, $between );
+
+			} else {
+
+				$output = implode( '', $outputs );
+			}
+		}
+		return $output;
+	}
+
+	public static function loop_through_acf_gallery_field( $atts, $content ) {
+
+		extract( shortcode_atts( array(
+			'field' => '',
+			'count' => '',
+			'start' => '',
+			'subfield' => '',
+			'sub' => '',
+			'columns' => '', 'pad' => '', 'between' => '', 
+		), $atts ));
+
+
+		// If in repeater or flexible content, get subfield by default
+		if ( self::$state['is_repeater_or_flex_loop']=='true' ) {
+			$sub = 'true';
+		}
+
+		// Backward compatibility
+		if (!empty($subfield)) {
+			$field = $subfield;
+			$sub = 'true';
+		}
+
+		if (empty($sub)) {
+			$images = get_field( $field );
+		} else {
+			$images = get_sub_field( $field );
+		}
+
+		$outputs = array();
+
+		if ( $images ) {
+
+			$index_now = 0;
+			if ( $start == '' ) $start='1';
+
+			foreach ( $images as $image ) {
+
+				self::$state['current_image'] = $image;
+				$index_now++;
+
+				if ( $index_now >= $start ) { /* Start loop */
+
+					if ( ( $count!= '' ) && ( $index_now >= ($start+$count) ) ) {
+							break;				/* If over count, break the loop */
+					}
+
+					$outputs[] = str_replace( '{COUNT}', $index_now, do_shortcode($content) );
+				}
+			}
+		}
+		if( !empty($outputs) && is_array($outputs)) {
+
+			if (!empty($columns))
+				$output = CCS_Loop::render_columns( $outputs, $columns, $pad, $between );
+			else
+				$output = implode( '', $outputs );
+		} else {
+			$output = $outputs;
+		}
+
+		self::$state['current_image'] = '';
+		return $output;
+	}
+
+	public static function get_image_details_from_acf_gallery( $atts ) {
+
+		extract(shortcode_atts(array(
+			'field' => '',
+			'size' => '',
+		), $atts));
+
+		if ( $field!='' ) {
+				$output = self::$state['current_image'][$field];
+		} else {
+
+			if ($size=='') {
+				$image_url = self::$state['current_image']['url'];
+			} else {
+				$image_url = self::$state['current_image']['sizes'][$size];
+			}
+
+			$output = '<img src="' . $image_url . '">';
+
+		}
+		return $output;
+	}
+
+	public static function if_get_row_layout( $atts, $content ) {
+
+		extract(shortcode_atts(array(
+			'name' => '',
+		), $atts));
+
+		if( get_row_layout() == $name ) {
+			return do_shortcode( $content );
+		} else {
+			return null;
+		}
+	}
+
+	public static function loop_relationship_field( $atts, $content ) {
+
+		extract( shortcode_atts( array(
+			'field' => '',
+			'subfield' => '',
+		), $atts ) );
+
+		$output = array();
+
+		// If in repeater or flexible content, get subfield by default
+		if ( self::$state['is_repeater_or_flex_loop']=='true' ) {
+			if (empty($subfield)) {
+				$subfield = $field;
+				$field = null;
+			}
+		}
+
+		if (!empty($field)) {
+			$posts = get_field($field);
+		} elseif (!empty($subfield)) {
+			$posts = get_sub_field($subfield);
+		} else return null;
+
+
+		if ($posts) {
+
+			self::$state['is_relationship_loop'] = 'true';
+
+			$index_now = 0;
+
+			if ( ! is_array($posts) ) {
+				$posts = array( $posts ); // Single post
+			}
+
+			foreach ($posts as $post) { // must be named $post
+
+				$index_now++;
+
+				self::$state['relationship_id'] = $post->ID;
+
+				$output[] = str_replace('{COUNT}', $index_now, do_shortcode($content));
+			}
+
+		}
+
+		self::$state['is_relationship_loop'] = 'false';
+
+		if (is_array($output))
+			$output = implode('', $output);
+		return $output;
+	}
+
+
+	/*====================================================================================================
+	 *
+	 * Live Edit shortcode (legacy)
+	 *
+	 *====================================================================================================*/
+
+	public static function call_live_edit($atts, $inside_content = null) {
+		extract(shortcode_atts(array(
+			'field' => '',
+			'admin' => '',
+			'editor' => '',
+			'edit' => '',
+			'only' => '',
+			'content' => '',
+			'title' => '',
+			'all' => '',
+		), $atts));
+
+		if( (function_exists('live_edit') && ( (current_user_can('edit_posts')) || ($all=='true') ) &&
+			($edit!='off')) ){
+
+			$edit_field = '';
+
+			if(($title!='false')&&($title!='off')) {
+				$edit_field .= 'post_title,';	
+			}
+			if(($content!='false')&&($content!='off')) {
+				$edit_field .= 'post_content,';	
+			}
+
+			if($admin!=''){
+				if ( current_user_can( 'manage_options' ) ) { // Admin user
+					$edit_field .= $admin;
+				} else { // Editor
+					if(($editor=='') && ($only=='')) { // Edit only for admin
+						return do_shortcode($inside_content);
+					}
+					if($editor!='') {
+						$edit_field .= $editor;
+					}
+					if($only != '') {
+						$edit_field = $only;
+					}
+				}
+			} else {			if($field != '') {
+					$edit_field .= $field;
+				}
+				if($only != '') {
+					$edit_field = $only;
+				}
+			}
+			$edit_field = trim($edit_field, ',');
+			$output = '<div ';
+			$output .= live_edit($edit_field);
+			$output .= '>';
+			$output .= do_shortcode($inside_content) . '</div>';
+
+			return $output;
+		} else {
+			return do_shortcode($inside_content);
+		}
+	}
+
+}
+
---
+++ includes/comments.php
@@ -0,0 +1,260 @@
+<?php
+
+/*====================================================================================================
+ *
+ * Comment shortcodes - [comment form] form/template/count
+ *
+ *====================================================================================================*/
+
+new CCS_Comments;
+
+class CCS_Comments {
+
+	public static $state;
+
+	function __construct() {
+
+		add_shortcode('comment', array($this, 'comment_shortcode') );
+		add_shortcode('comments', array($this, 'comment_shortcode') );
+
+		self::$state['is_comments_loop'] = false;
+	}
+
+	function comment_shortcode( $atts, $content, $tag ) {
+
+		extract(shortcode_atts(array(
+			'template' => '',
+			'id' => '',
+			'count' => '',
+			'format' => '',
+			'date_format' => '',
+			'words' => '',
+			'more' => '&hellip;',
+			'length' => '',
+		), $atts));
+
+		// In a comments loop?
+
+		$in_loop = isset(self::$state['is_comments_loop']) ? self::$state['is_comments_loop'] : false;
+
+		if ( $in_loop ) {
+
+			// Display comment fields
+
+			$out = null;
+			$comment = self::$state['current_comment'];
+
+			if (empty($comment)) return;
+
+			$fields = array(
+				'ID', 'post_ID', 'author', 'author_email', 'author_url', 'date',
+				'content', 'user_id',
+				'title', 'url', 'title_link', 'author_link'
+			);
+
+			if( is_array( $atts ) )
+				$atts = array_flip( $atts ); // check for parameters without value
+
+			$post_id = $comment->comment_post_ID;
+
+			foreach ($fields as $field) {
+
+				$arg_field = strtolower($field);
+				$arg_field = str_replace('_', '-', $field);
+
+				if ($arg_field=='user-id')
+					$field = 'user_id';
+				else
+					$field = 'comment_'.$field; // name of property in comment object
+
+				// Check first parameter [comment ~]
+
+				if (isset($atts[$arg_field])) {
+
+					switch ($arg_field) {
+						case 'title':
+							$out = get_the_title($post_id);
+							break;
+						case 'url':
+							$out = get_permalink($post_id);
+							break;
+						case 'title-link':
+							$title = get_the_title($post_id);
+							$url = get_permalink($post_id);
+							// $out = '<a href="'.$url.'">'.$title.'</a>';
+							break;
+						case 'author-link':
+							$title = isset($comment->comment_author) ? $comment->comment_author : null;
+							$url = isset($comment->comment_author_url) ? $comment->comment_author_url : null;
+							// $out = '<a href="'.$url.'">'.$title.'</a>';
+							break;
+						case 'content':
+							if (isset($comment->{$field}))
+								$out = $comment->{$field};
+							if (empty($format)) $format='true'; // Format content by default
+							break;
+						default:
+							if (isset($comment->{$field}))
+								$out = $comment->{$field};
+							break;
+					}
+				}
+
+			}
+
+			if (!empty($words)) {
+				$out = wp_trim_words( $out, $words, $more );
+			}
+			if (!empty($length)) {
+				$the_excerpt = $out;
+				$the_excerpt = strip_tags(strip_shortcodes($the_excerpt)); //Strips tags and images
+				$out = mb_substr($the_excerpt, 0, $length, 'UTF-8');
+			}
+
+			// Wrap in link after trimming?
+			if (isset($atts['title-link']) || isset($atts['author-link'])) {
+				if (!empty($title) && !empty($url))
+					$out = '<a href="'.$url.'">'.$title.'</a>';
+				elseif (!empty($title))
+					$out = $title; // no link found
+			}
+
+			if ( !empty($out) && !empty($date_format) ) {
+				$out = date($date_format, strtotime($out));
+			} elseif ( $format=='true' ) {
+				$out = apply_filters('the_content', $out);
+			}
+
+			return $out;
+		}
+
+		// Start a comments loop?
+		if ( !empty($count) || !empty($id) ||
+			 ( ($tag=='comments') && !empty($content) ) ) {
+
+			$out = '';
+			self::$state['is_comments_loop'] = true;
+			if ((empty($count)) || ($count=='all')) $count = 999;
+			$atts['number'] = $count;
+			if ($id=='this') {
+				$atts['post_id'] = get_the_ID();
+			} elseif (!empty($id)) {
+				$atts['post_id'] = $id;
+			}
+
+			// Pass arguments
+			$defaults = array(
+				'number' => '',
+				'offset' => '',
+				'orderby' => '',
+				'order' => 'DESC',
+				'parent' => '',
+				'post_id' => '',
+				'post_author' => '',
+				'post_name' => '',
+				'post_parent' => '',
+				'post_status' => '',
+				'post_type' => '',
+				'status' => 'approve',
+				'type' => '',
+				'user_id' => '',
+			);
+
+			$args = array();
+			foreach ($defaults as $key => $value) {
+				if (!empty($atts[$key])) {
+					if ($key=='status' && $atts[$key]=='all') {
+						// Don't set status value
+					} else {
+						$args[$key] = $atts[$key];
+					}
+				} elseif (!empty($value)) {
+					$args[$key] = $value;
+				}
+			}
+
+			$comments = get_comments( $args );
+
+			// Loop through each comment
+			foreach ($comments as $comment) {
+				self::$state['current_comment'] = $comment;
+				$out .= do_shortcode( $content );
+			}
+			self::$state['is_comments_loop'] = false;
+			return $out;
+		}
+
+
+		if( is_array( $atts ) )
+			$atts = array_flip( $atts ); // check for parameters without value
+
+
+		// Comments template?
+
+		if ( ( ($tag=='comments') || isset( $atts['template'] ) || (!empty($template)))
+			&& (empty($id))
+			)
+
+		{
+
+			$dir = '';
+	/*		if (isset($atts['dir'])) {
+				$dir = do_shortcode('[url '.$atts['dir'].']/');
+			}
+	*/
+			if (empty($template)) $template = '/comments.php';
+			if (isset($template[0]) && ($template[0]!='/'))
+				$template = '/'.$template;
+
+			$file = $dir.$template;
+	/*
+			echo 'file: '.$file.'<br>';
+	// filter 'comments_template' gets this value
+			echo 'style: '.STYLESHEETPATH . $file.'<br>';
+			echo 'template: '.TEMPLATEPATH . $file .'<br>';
+	*/
+			$content = self::return_comments_template($dir.$template);
+
+			return $content;
+		}
+
+		if( isset( $atts['form'] ) ) {
+			$content = self::return_comment_form();
+			return $content;
+		}
+		if( isset( $atts['count'] ) ) {
+			return get_comments_number();
+		}
+		if( isset( $atts['total'] ) ) {
+			return CCS_Loop::$state['comment_count'];
+		}
+	}
+
+	function return_comment_form() {
+
+		ob_start();
+
+		comment_form( $args = array(
+			'id_form'           => 'commentform',  // that's the wordpress default value! delete it or edit it ;)
+			'id_submit'         => 'commentsubmit',
+			'title_reply'       => __( '' ),  // Leave a Reply - that's the wordpress default value! delete it or edit it ;)
+			'title_reply_to'    => __( '' ),  // Leave a Reply to %s - that's the wordpress default value! delete it or edit it ;)
+			'cancel_reply_link' => __( 'Cancel Reply' ),  // that's the wordpress default value! delete it or edit it ;)
+			'label_submit'      => __( 'Post Comment' ),  // that's the wordpress default value! delete it or edit it ;)
+				
+			'comment_field' =>  '<p><textarea placeholder="" id="comment" class="form-control" name="comment" cols="45" rows="8" aria-required="true"></textarea></p>', 
+				
+			'comment_notes_after' => ''
+		));
+		$form = ob_get_clean();
+	    return $form;
+	}
+
+	function return_comments_template($file) {
+
+		ob_start();
+		comments_template($file);
+		return ob_get_clean(); 
+	}
+
+}
