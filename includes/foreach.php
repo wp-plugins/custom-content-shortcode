c60e51ab4e93b630a08b7cc4ccceb69b241b9e22
miyarakira <miyarakira@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-12-07 11:15:01 +0000

    tagging 1.5.4
    git-svn-id: https://plugins.svn.wordpress.org/custom-content-shortcode/tags/1.5.4@1039764 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ includes/foreach.php
@@ -0,0 +1,217 @@
+<?php
+
+/*========================================================================
+ *
+ * For each taxonomy
+ * 
+ * [for each="category"]
+ * [each name,id,slug]
+ * 
+ *=======================================================================*/
+
+
+new CCS_ForEach;
+
+class CCS_ForEach {
+
+	public static $state;
+
+	function __construct() {
+
+		self::$state['is_for_loop'] = false;
+
+		add_action( 'init', array( $this, 'register' ) );
+	}
+
+	function register() {
+		add_shortcode( 'for', array( $this, 'for_shortcode' ) );
+		add_shortcode( 'each', array( $this, 'each_shortcode' ) );
+		add_shortcode( 'for-loop', array( $this, 'for_loop_status' ) );
+	}
+
+	function for_shortcode( $atts, $content = null, $shortcode_name ) {
+
+		$args = array(
+			'each' => '',
+			'orderby' => '',
+			'order' => '',
+			'count' => '',
+			'parent' => '',
+			'current' => '',
+			'trim' => '',
+			'empty' => 'false',
+			'exclude' => ''
+		);
+
+		extract( shortcode_atts( $args , $atts, true ) );
+
+		self::$state['is_for_loop'] = true;
+		if ($each=='tag') $each='post_tag';
+		$out = '';
+
+		// Loop through taxonomies
+
+		if ((CCS_Loop::$state['is_loop']=="true") || ($current=="true")) {
+
+			if ($current=="true") {
+
+				$post_id = get_the_ID();
+
+			} else {
+
+				// Inside [loop]
+				$post_id = CCS_Loop::$state['current_post_id'];
+			}
+
+			$taxonomies = wp_get_post_terms(
+				$post_id,
+				$each, array(
+				'orderby' => $orderby,
+				'order' => $order,
+				'number' => $count,
+				'hide_empty' => ($empty=='true' ? 0 : 1),
+				) );
+
+			// Current and parent parameters together
+
+			if (!empty($parent)) {
+				
+				if ( is_numeric($parent) ) {
+
+					/* Get parent term ID */
+					$parent_term_id = $parent;
+
+				} else {
+
+					/* Get parent term ID from slug */
+					$term = get_term_by( 'slug', $parent, $each );
+					if (!empty($term))
+						$parent_term_id = $term->term_id;
+					else $parent_term_id = null;
+				}
+
+				if (!empty($parent_term_id)) {
+					/* Filter out terms that do not have the specified parent */
+					foreach($taxonomies as $key => $term) {
+						if ($term->parent != $parent_term_id) {
+							unset($taxonomies[$key]);
+						}
+					}
+
+				}
+			}
+
+		} else {
+
+			if (empty($parent)) {
+
+				$taxonomies = get_terms( $each, array(
+					'orderby' => $orderby,
+					'order' => $order,
+					'number' => $count,
+					'hide_empty' => ($empty=='true' ? 0 : 1),
+					) );
+
+			} else {
+
+				/* Get parent term ID from slug */
+
+				if ( is_numeric($parent) ) {
+
+					$parent_term_id = $parent;
+
+				} else {
+					$term = get_term_by( 'slug', $parent, $each );
+					if (!empty($term))
+						$parent_term_id = $term->term_id;
+					else $parent_term_id = null;
+				}
+
+				if (!empty($parent_term_id)) {
+					/* Get direct children */
+					$taxonomies = get_terms( $each, array(
+						'orderby' => $orderby,
+						'order' => $order,
+						'number' => $count,
+						'parent' => $parent_term_id,
+						'hide_empty' => ($empty=='true' ? 0 : 1),
+						) );
+
+				} else { /* No parent found */
+					$taxonomies = null;
+				}
+			}
+		}
+
+
+		if (is_array($taxonomies)) {
+
+			self::$state['each']['type']='taxonomy';
+			self::$state['each']['taxonomy']=$each;
+
+			$excludes = CCS_Loop::explode_list( $exclude );
+
+			foreach ($taxonomies as $term_object) {
+
+				// Exclude IDs or slugs
+
+				$condition = true;
+				foreach ($excludes as $exclude) {
+					if ( is_numeric($exclude) ) {
+						 // Exclude ID
+						if ( $exclude == $term_object->term_id ) {
+							$condition = false;
+						}
+					} else {
+						 // Exclude slug
+						if ( $exclude == $term_object->slug ) {
+							$condition = false;
+						}
+					}
+				}
+
+				if ($condition) {
+					self::$state['each']['id'] = $term_object->term_id;
+					self::$state['each']['name'] = $term_object->name;
+					self::$state['each']['slug'] = $term_object->slug;
+					self::$state['each']['description'] = $term_object->description;
+
+					$term_link = get_term_link( $term_object );
+					if ( is_wp_error( $term_link ) ) $term_link = null;
+
+					self::$state['each']['url'] = $term_link;
+
+					self::$state['each']['name-link'] = '<a href="'.self::$state['each']['url'].'">'
+						. self::$state['each']['name'].'</a>';
+
+					$out .= do_shortcode($content);
+				}
+			}
+		}
+
+		// Trim final output
+
+		if (!empty($trim)) {
+			if ($trim=='true') $trim = null;
+			$out = trim($out, " \t\n\r\0\x0B,".$trim);
+		}
+
+		self::$state['is_for_loop'] = false;
+		self::$state['each'] = null;
+
+		return $out;
+	}
+
+	function each_shortcode( $atts, $content = null, $shortcode_name ) {
+
+		if ( !self::$state['is_for_loop'] )
+				return; // Must be inside a for loop
+
+		$field = isset($atts[0]) ? $atts[0] : 'name'; // Default: name
+        $out = isset( self::$state['each'][$field] ) ? self::$state['each'][$field] : null;
+
+        return $out;
+	}
+
+}
+
---
+++ includes/url.php
@@ -0,0 +1,145 @@
+<?php
+
+/*====================================================================================================
+ *
+ * Relative URL shortcodes - [url site/theme/child/views/content/uploads]
+ *
+ *====================================================================================================*/
+
+new CCS_URL;
+
+class CCS_URL {
+
+	public static $urls; // Store URLs
+
+	function __construct() {
+
+		self::$urls = array();
+
+		add_shortcode( 'url', array($this, 'url_shortcode') );
+		add_shortcode( 'redirect', array($this, 'redirect_shortcode') );
+	}
+    
+    public static function url_shortcode( $atts ) {
+
+    	$urls = self::$urls;
+    	$url = null;
+
+		extract(shortcode_atts(array(
+			'login' => '',
+			'logout' => '',
+			'go' => '',
+		), $atts));
+
+		if (empty($atts)) $atts[0] = 'site';
+
+		$arg = $atts[0];
+
+        if ( is_array($atts ) ) $atts = array_flip( $atts ); // Allow checking empty parameters
+
+        // Find where to go after login/out
+
+		if ( ($go=='here') || (isset($atts['logout']) && empty($go)) ) {
+
+			global $wp; $go=home_url($wp->request); // Current page URL
+
+		} elseif ( !empty($go) ) {
+
+			if ($go=='home') {
+
+				$go = isset($urls['home']) ? $urls['home'] : ($urls['home'] = get_option('home'));
+
+			} elseif( ($arg=='login') || ($arg=='logout') ) {
+
+				if( !strpos ($go,"." ) ) {
+
+					// Go to page URL by slug
+					$go = do_shortcode('[content name="'.$go.'" field="url"]');
+				}
+			}
+		}
+
+		switch ($arg) {
+
+			case 'wordpress':
+				$url = isset($urls[$arg]) ? $urls[$arg] : ($urls[$arg] = get_option('siteurl'));
+				break;
+
+			case 'content':
+				$url = isset($urls[$arg]) ? $urls[$arg] : ($urls[$arg] = content_url());
+				break;
+
+			case 'parent': // Parent Theme
+			case 'theme':
+				$url = isset($urls[$arg]) ? $urls[$arg] : ($urls[$arg] = get_bloginfo('template_directory'));
+				break;
+
+			case 'child':
+				$url = isset($urls[$arg]) ? $urls[$arg] : ($urls[$arg] = get_bloginfo('stylesheet_directory'));
+				break;
+
+			case 'uploads':
+
+				if (isset($urls[$arg]))
+					$url = $urls[$arg];
+				else {
+
+					// Get uploads directory
+
+					$upload_dir = wp_upload_dir();
+					if( !$upload_dir['error'] ) {
+						$url = ($urls[$arg] = $upload_dir['baseurl']);
+					} elseif ( $url = get_option( 'upload_url_path' ) ) {
+						// Prior to WordPress 3.5, this was set in Settings > Media > Full URL path to files
+						// In WordPress 3.5+ this is now hidden
+						$urls[$arg] = $url;
+					} else {
+						$url = ($urls[$arg] = get_option('siteurl').'/'.get_option('upload_path') );
+					}
+				}
+				break;
+
+			case 'layout':
+			case 'views':
+				$url = isset($urls[$arg]) ? $urls[$arg] : ($urls[$arg] = content_url().'/'.$arg);
+				break;
+
+			case 'login':
+				$url = wp_login_url( $go ); // Don't store this, as go parameter could be different
+				break;
+
+			case 'logout':
+				$url = wp_logout_url( $go );
+				break;
+
+
+			case 'home':
+			default:
+				$url = isset($urls['home']) ? $urls['home'] : get_option('home');
+				break;
+		}
+
+		self::$urls = $urls; // Store it for later
+
+        // unslash?
+
+        return untrailingslashit( $url );
+    }
+
+
+	/* Redirect to another page */
+
+	function redirect_shortcode( $atts, $content ) {
+
+		if (empty($content)) {
+			// First argument as URL
+			$content = isset($atts['go']) ? $atts['go'] : null;
+			if (!empty($content))
+				echo "<script> window.location = '" . strip_tags( $content ) . "'; </script>";
+		} else {
+			// Shortcode content as URL
+			echo "<script> window.location = '" . strip_tags( do_shortcode(trim($content)) ) . "'; </script>";
+		}
+	}
+
+}
