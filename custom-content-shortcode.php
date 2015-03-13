f41360ab3f170a55003646ec41d672d0f1f3b2f6
miyarakira <miyarakira@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-03-13 04:06:15 +0000

    1.8.5
    git-svn-id: https://plugins.svn.wordpress.org/custom-content-shortcode/tags/1.8.5@1111683 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- custom-content-shortcode.php
+++ custom-content-shortcode.php
@@ -3,7 +3,7 @@
 Plugin Name: Custom Content Shortcode
 Plugin URI: http://wordpress.org/plugins/custom-content-shortcode/
 Description: Display posts, pages, custom post types, custom fields, files, images, comments, attachments, menus, or widget areas
-Version: 1.5.6
+Version: 1.8.5
 Shortcodes: loop, content, field, taxonomy, if, for, each, comments, user, url, load
 Author: Eliot Akira
 Author URI: eliotakira.com
@@ -32,7 +32,7 @@ class CCS_Plugin {
 	 *
 	 * Load settings
 	 *
-	 *=======================================================================*/
+	 */
 
 	function load_settings() {
 
@@ -44,18 +44,15 @@ class CCS_Plugin {
 
 			// Default settings
 
-			self::$settings['load_acf_module'] = 'on';
-			self::$settings['load_bootstrap_module'] = 'on';
-			self::$settings['load_file_loader'] = 'on';
-			self::$settings['load_gallery_field'] = 'on';
-
-			self::$settings['load_mobile_detect'] = 'off';
-
-			self::$settings['shortcodes_in_widget'] = 'on';
-
-			self::$settings['raw_shortcode'] = 'off';
-			self::$settings['move_wpautop'] = 'off';
-			self::$settings['shortcode_unautop'] = 'off';
+			self::$settings['load_acf_module']       = 'on';
+			self::$settings['load_bootstrap_module'] = 'off';
+			self::$settings['load_file_loader']      = 'on';
+			self::$settings['load_gallery_field']    = 'on';
+			self::$settings['load_mobile_detect']    = 'off';
+			self::$settings['shortcodes_in_widget']  = 'on';
+			self::$settings['raw_shortcode']         = 'off';
+			self::$settings['move_wpautop']          = 'off';
+			self::$settings['shortcode_unautop']     = 'off';
 
 			update_option( $option_key, self::$settings );
 		}
@@ -66,34 +63,37 @@ class CCS_Plugin {
 	 *
 	 * Load main and optional modules
 	 *
-	 *=======================================================================*/
+	 */
 
 	function load_module( $module ) {
 
-		require_once (CCS_PATH.'/includes/'.$module.'.php');
+		require_once ( CCS_PATH.'/includes/'.$module.'.php' );
 
 	}
 
 	function load_main_modules() {
 
-		$main_modules = array(
-			'content',		// Content shortcode
-			'loop',			// Loop shortcode
-			'attached',		// Attachment loop
-			'comments',		// Comments shortcode
-			'related',		// Related posts loop
-			'user',			// User shortcodes
-			'url',			// URL shortcode
-			'foreach',		// For/each loop
-			'if',			// If shortcode
-			'docs',			// Documentation under Settings -> Custom Content
-			'cache',		// Cache shortcode
-			'format',		// Format shortcodes: br, p, x, clean, direct, format
-			'wck',			// WCK support
-		//	'widget'		// Widget shortcode (not ready)
+		$modules = array(
+			'core/content',			// Content shortcode
+			'core/loop',			// Loop shortcode
+			'docs/docs',			// Documentation under Settings -> Custom Content
+			'modules/attached',		// Attachment loop
+			'modules/cache',		// Cache shortcode
+			'modules/comments',		// Comments shortcode
+			'modules/foreach',		// For/each loop
+			'modules/format',		// Format shortcodes: br, p, x, clean, direct, format
+			'modules/if',			// If shortcode
+			'modules/related',		// Related posts loop
+			'modules/url',			// URL shortcode
+			'modules/user',			// User shortcodes
+			'optional/wck',			// WCK support
+
+			// 'widget'				// Widget shortcode (not ready)
+			// 'optional/shortcake',	// Shortcake: shortcode UI @testing
+
 		);
 
-		foreach ($main_modules as $module) {
+		foreach ($modules as $module) {
 
 			$this->load_module( $module );
 		}
@@ -103,7 +103,7 @@ class CCS_Plugin {
 	 *
 	 * Optional modules
 	 *
-	 *=======================================================================*/
+	 */
 
 	function load_optional_modules() {
 
@@ -111,13 +111,13 @@ class CCS_Plugin {
 
 			// Option name => module name
 
-			'load_gallery_field'	=> 'gallery',		// Gallery field
-			'load_acf_module'		=> 'acf',			// Advanced Custom Fields support
-			'load_file_loader'		=> 'load',			// Load HTML, CSS, JS fields
-			'load_mobile_detect'	=> 'mobile',		// Mobile detect shortcodes
-			'load_bootstrap_module'	=> 'bootstrap',		// Bootstrap support
-			'shortcode_unautop'		=> 'unautop',		// Shortcode unautop
-			'raw_shortcode'			=> 'raw',			// [raw]
+      'load_acf_module'       => 'optional/acf',			  // Advanced Custom Fields support
+      'load_bootstrap_module' => 'optional/bootstrap',  // Bootstrap support
+      'load_gallery_field'    => 'optional/gallery',		// Gallery field
+      'load_file_loader'      => 'optional/load',			  // Load HTML, CSS, JS fields
+      'load_mobile_detect'    => 'optional/mobile',		  // Mobile detect shortcodes
+      'raw_shortcode'         => 'optional/raw',        // [raw]
+      'shortcode_unautop'     => 'optional/unautop',    // Shortcode unautop
 		);
 
 		foreach ($optional_modules as $option => $module) {
@@ -134,7 +134,7 @@ class CCS_Plugin {
 	 *
 	 * Set up WP filters
 	 *
-	 *=======================================================================*/
+	 */
 	
 	function setup_wp_filters() {
 
@@ -144,7 +144,7 @@ class CCS_Plugin {
 		 *
 		 * Enable shortcodes in widget
 		 *
-		 *=======================================================================*/
+		 */
 
 		if ( isset( $settings['shortcodes_in_widget'] ) &&
 			($settings['shortcodes_in_widget'] == "on") ) {
@@ -158,13 +158,13 @@ class CCS_Plugin {
 
 		/*========================================================================
 		 *
-		 * Move wpautop filter to after shortcode processing (legacy)
+		 * Move wpautop filter to after shortcode processing (deprecated)
 		 * 
 		 * User feedback suggests some themes/plugins don't work well with the
 		 * filter moved, because they assume default priority. Instead, use [raw]
 		 * or edit code outside of post editor.
 		 *
-		 *=======================================================================*/
+		 */
 
 		if ( isset( $settings['move_wpautop'] ) &&
 			($settings['move_wpautop'] == "on") ) {
@@ -188,7 +188,7 @@ class CCS_Plugin {
  *
  * Global helper functions
  *
- *=======================================================================*/
+ */
 
 if (!function_exists('do_short')) {
 	function do_short($content) {
@@ -201,10 +201,10 @@ if (!function_exists('start_short')) {
 		ob_start();
 	}
 }
+
 if (!function_exists('end_short')) {
 	function end_short() {
-		$out = ob_get_clean();
-		do_short($out);
+		do_short( ob_get_clean() );
 	}
 }
 
